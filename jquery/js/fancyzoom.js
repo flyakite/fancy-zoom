492b131eca77d292ab6d62f48bb9d624d596166f
Shih-Wen Su <sushi@summeridea.com>
2012-08-11 14:45:28 +0800

    dynamic load target image on click

--- jquery/js/fancyzoom.js
+++ jquery/js/fancyzoom.js
@@ -1,9 +1,9 @@
 (function($){
 $.fn.fancyzoom = function(options){
 
-    var options     = options || {};
+    var options = options || {};
     var directory = options && options.directory ? options.directory : 'images';
-    var zooming     = false;
+    var zooming = false;
 
     if ($('#zoom').length == 0) {
         var ext = $.browser.msie ? 'gif' : 'png';
@@ -16,7 +16,8 @@ $.fn.fancyzoom = function(options){
 
         $('html').click(function(e){if($(e.target).parents('#zoom:visible').length == 0) hide();});
         $(document).keyup(function(event){
-                if (event.keyCode == 27 && $('#zoom:visible').length > 0) hide();
+            if (event.keyCode == 27 && $('#zoom:visible').length > 0) 
+                hide();
         });
 
         $('#zoom_close').click(hide);
@@ -27,45 +28,49 @@ $.fn.fancyzoom = function(options){
         zoom_content = $('#zoom_content');
 
     this.each(function(i) {
-        $($(this).attr('href')).hide();
+        //$($(this).attr('href')).hide();
         $(this).click(show);
     });
 
     return this;
 
     function show(e) {
-        if (zooming) return false;
-		zooming                 = true;
-		var content_div = $($(this).attr('href'));
-    	var zoom_width    = options.width;
-		var zoom_height = options.height;
-
-		var width             = window.innerWidth || (window.document.documentElement.clientWidth || window.document.body.clientWidth);
-    	var height            = window.innerHeight || (window.document.documentElement.clientHeight || window.document.body.clientHeight);
-    	var x                     = window.pageXOffset || (window.document.documentElement.scrollLeft || window.document.body.scrollLeft);
-    	var y                     = window.pageYOffset || (window.document.documentElement.scrollTop || window.document.body.scrollTop);
+        if (zooming){
+            return false;
+        } 
+		zooming = true;
+		var src = $(this).attr('href'),
+		    $img = $('<img />').attr('src', src),
+		    $content = $('<div />').addClass('garbage').append($img),
+    	    zoom_width = options.width,
+		    zoom_height = options.height;
+	    $content.appendTo('body').hide();
+		var width   = window.innerWidth || (window.document.documentElement.clientWidth || window.document.body.clientWidth);
+    	var height  = window.innerHeight || (window.document.documentElement.clientHeight || window.document.body.clientHeight);
+    	var x = window.pageXOffset || (window.document.documentElement.scrollLeft || window.document.body.scrollLeft);
+    	var y = window.pageYOffset || (window.document.documentElement.scrollTop || window.document.body.scrollTop);
     	var window_size = {'width':width, 'height':height, 'x':x, 'y':y}
 
-		var width                            = (zoom_width || content_div.width());
-		var height                         = (zoom_height || content_div.height());
-		var d                                    = window_size;
-
+		var width = (zoom_width || $content.width());
+		var height = (zoom_height || $content.height());
+		var d = window_size;
 		// ensure that newTop is at least 0 so it doesn't hide close button
-		var newTop                         = Math.max((d.height/2) - (height/2) + y, 0);
-		var newLeft                        = (d.width/2) - (width/2);
-		var curTop                         = e.pageY;
-		var curLeft                        = e.pageX;
-
+		var newTop = Math.max((d.height/2) - (height/2) + y, 0);
+		var newLeft = (d.width/2) - (width/2);
+		var curTop = e.pageY;
+		var curLeft = e.pageX;
+        
+        $('.garbage').remove()
 		zoom_close.attr('curTop', curTop);
 		zoom_close.attr('curLeft', curLeft);
 		zoom_close.attr('scaleImg', options.scaleImg ? 'true' : 'false');
 
         $('#zoom').hide().css({
-			position	: 'absolute',
-			top				: curTop + 'px',
-			left			: curLeft + 'px',
-			width         : '1px',
-			height        : '1px'
+			position : 'absolute',
+			top : curTop + 'px',
+			left : curLeft + 'px',
+			width : '1px',
+			height : '1px'
 		});
 
         zoom_close.hide();
@@ -75,7 +80,7 @@ $.fn.fancyzoom = function(options){
         }
 
 		if (options.scaleImg) {
-    		zoom_content.html(content_div.show());
+    		zoom_content.html($content.html());
     		$('#zoom_content img').css('width', '100%');
 		} else {
 		    zoom_content.html('');
@@ -87,9 +92,9 @@ $.fn.fancyzoom = function(options){
             opacity : "show",
             width     : width,
             height    : height
-        }, 500, null, function() {
+        }, 200, null, function() {
             if (options.scaleImg != true) {
-        		zoom_content.html(content_div.show());
+        		zoom_content.html($content.html());
     		}
 			zoom_close.show();
 			zooming = false;
@@ -111,7 +116,7 @@ $.fn.fancyzoom = function(options){
             opacity : "hide",
             width     : '1px',
             height    : '1px'
-        }, 500, null, function() {
+        }, 200, null, function() {
             if (zoom_close.attr('scaleImg') == 'true') {
         		zoom_content.html('');
     		}
